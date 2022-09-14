c42a2828a1af8bb7b24a62e95912f5aa384283cc
tbtoanst <71660591+tbtoanst@users.noreply.github.com>
2022-09-14 11:31:13 +0700

    1 file modified

--- src/app/tabs/tab-panel.component.ts
+++ src/app/tabs/tab-panel.component.ts
@@ -1,21 +1,30 @@
-import { Component, Input, TemplateRef, ViewChild } from "@angular/core";
-import { TabGroupComponent } from "./tab-group.component";
+import {
+  Component,
+  ContentChild,
+  Input,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
+import { TabGroupComponent } from './tab-group.component';
 
 @Component({
   selector: 'tab-panel',
-  template:`
+  template: `
     <ng-template>
       <ng-content></ng-content>
     </ng-template>
-  `
+  `,
 })
-export class TabPanelComponent{
+export class TabPanelComponent {
   @Input() title: string;
-  @ViewChild(TemplateRef, {static: true}) panelBody: TemplateRef<unknown>;
-  
+  @ViewChild(TemplateRef, { static: true }) implicitBody: TemplateRef<unknown>;
+  @ContentChild(TemplateRef, { static: true })
+  explicitBody: TemplateRef<unknown>;
+
   constructor(private tabGroup: TabGroupComponent) {}
 
   ngOnInit() {
+    console.log(this.explicitBody);
     this.tabGroup.addTab(this);
   }
 }
